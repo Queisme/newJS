a25c51ffe214b1562ee5bc11f50df057a8a24538
Queisme <queiskey@gmail.com>
2024-09-20 08:31:52 -0500

    update

---
+++ course/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,142 @@
+// Data types
+// Values are Objects or Primitive Values
+
+// 7 primitive data types
+// 1. Number
+// 2. String
+// 3. Boolean
+// 4. BigInt
+// 5. Symbol
+// 6. Undefined
+// 7. Null
+
+// BMI = mass / height ** 2
+// (mass in kg and height in meter)
+
+//const markMass = 78;
+//const markHeight = 1.69;
+//const johnMass = 92;
+//const johnHeight = 1.95;
+
+//const markBMI = markMass / markHeight ** 2;
+//console.log(markBMI);
+//const johnBMI = johnMass / johnHeight ** 2;
+//console.log(johnBMI);
+//const markHigherBMI = markBMI > johnBMI;
+//console.log(markHigherBMI);
+
+// Coding Challenge #1
+// Test Data 1
+// Mark higher BMI
+
+// Test Data 2
+// John higher BMI
+
+//const markWeight = 95;
+//const markLength = 1.88;
+//const johnWeight = 85;
+//const johnLength = 1.76;
+
+//const markBMI2 = markWeight / markLength ** 2;
+//console.log(markBMI2);
+//const johnBMI2 = johnWeight / johnLength ** 2;
+//console.log(johnBMI2);
+
+//const markHigherBMI2 = markBMI2 > johnBMI2;
+//console.log(markHigherBMI2);
+/*
+const firstName = 'Que';
+const job = 'coder';
+const birthYear = 1990;
+const year = 2037;
+
+console.log(
+  `I'm ${firstName}, I am ${year - birthYear} years old and I'm a ${job}.`
+);
+*/
+/*
+console.log(`String
+  with
+  multiple
+  lines.`);
+*/
+/*
+const age = 13;
+
+if (age >= 18) {
+  console.log('You are old enough to try for your drivers license');
+} else {
+  console.log(
+    `You are not old enough to try for your drivers license yet but you will be in ${
+      18 - age
+    } years.`
+  );
+}
+*/
+/*
+const birthYear = 2011;
+
+let century;
+
+if (birthYear <= 2000) {
+  century = '20th';
+} else {
+  century = '21st';
+}
+console.log(century);
+*/
+
+// Coding Challenge #2
+// Use BMI example from Challenge #1
+/*
+const markMass = 78;
+const markHeight = 1.69;
+const johnMass = 92;
+const johnHeight = 1.95;
+
+const marklongBMI = markMass / markHeight ** 2;
+const markBMI = marklongBMI.toFixed(2);
+console.log(markBMI);
+const johnlongBMI = johnMass / johnHeight ** 2;
+const johnBMI = johnlongBMI.toFixed(2);
+console.log(johnBMI);
+const markHigherBMI = markBMI > johnBMI;
+console.log(markHigherBMI);
+const markWeight = 95;
+const markLength = 1.88;
+const johnWeight = 85;
+const johnLength = 1.76;
+
+const marklongBMI2 = markWeight / markLength ** 2;
+const markBMI2 = marklongBMI2.toFixed(2);
+console.log(markBMI2);
+const johnlongBMI2 = johnWeight / johnLength ** 2;
+const johnBMI2 = johnlongBMI2.toFixed(2);
+console.log(johnBMI2);
+
+const markHigherBMI2 = markBMI2 > johnBMI2;
+console.log(markHigherBMI2);
+
+if (markBMI > johnBMI) {
+  console.log(
+    `Mark's BMI (${markBMI}) is higher than John's BMI (${johnBMI}).`
+  );
+} else {
+  console.log(
+    `John's BMI (${johnBMI}) is higher than Mark's BMI (${markBMI}).`
+  );
+}
+
+if (markBMI2 > johnBMI2) {
+  console.log(
+    `Mark's BMI (${markBMI2}) is higher than John's BMI (${johnBMI2}).`
+  );
+} else {
+  console.log(
+    `John's BMI (${johnBMI2}) is higher than Mark's BMI (${markBMI2}).`
+  );
+}
+*/
+
+//const inputYear = '1990';
+//console.log(inputYear + 18, +inputYear + 18);
